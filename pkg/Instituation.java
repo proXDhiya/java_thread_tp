a59c92222c07dc8db65c82c47f8820f32090a361
proXDhiya <prox.dhiya@gmail.com>
2021-12-13 07:14:42 +0100

    add global var

--- pkg/Instituation.java
+++ pkg/Instituation.java
@@ -1,5 +1,8 @@
 package pkg;
 interface Instituation {
+    public static int NumSeance = -1;
+    public static String SeanceType = "";
+
     public int AfficherNum(int num);
     public int AfficherSeance(String str);
 }
